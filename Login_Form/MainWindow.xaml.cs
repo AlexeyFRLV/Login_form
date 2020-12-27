1bcb84675ae6577fa9b6c7b8ce24245c25fc2fa0
Alexey Frolov <foritlearning@gmail.com>
2020-12-27 17:05:18 +0300

    Collapse, close and drag the form

--- Login_Form/MainWindow.xaml.cs
+++ Login_Form/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Login_Form
 {
@@ -7,6 +8,32 @@ namespace Login_Form
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExitButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void LogoContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
